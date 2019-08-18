ad47931af8e2c6d08ea4b5b16fe147ae9b18a664
unknown <serdar.plt21@gmail.com>
2019-08-18 22:01:56 +0300

    json editing

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -1,9 +1,17 @@
 class Product {
+  String id;
   String title;
+  String brand;
+  String image;
+  String price;
 
-  Product(this.title);
+  Product(this.id, this.title, this.brand, this.image, this.price);
 
   Product.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     title = json['title'];
+    brand = json['brand'];
+    image = json['image'];
+    price = json['price'];
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/pages/explore.dart';
+import 'package:ecommerce_app/pages/search_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightGreen,
         fontFamily: 'Josefin',
       ),
-      home: ExplorePage(),
+      home: SearchPage(),
     );
   }
 }
